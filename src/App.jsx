383e6c0f398234533db8252fc1197bbfdc85696e
sadaan <ansarisadaan72@gmail.com>
2024-03-05 16:25:58 +0530

    added id in section

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import './App.scss';
+// import Test from './Test';
 import Navbar from './components/navbar/Navbar';
 
 function App() {
   
   return (
       <div>
-        <section><Navbar /></section>
+        <section id='Homepage'><Navbar /></section>
         <section>Parallex</section>
-        <section>Services</section>
-        <section>Parallex</section>
-        <section>Portfolio 1</section>
+        <section id='Services'>Services</section>
+        <section >Parallex</section>
+        <section id='Portfolio'>Portfolio 1</section>
         <section>Portfolio 2</section>
         <section>Portfolio 3</section>
-        <section>Contact</section>
+        <section id='Contact'>Contact</section>
+        {/* <Test />
+        <Test /> */}
       </div>
   )
 }
