699480250c688c90e1c3a9aa209786a0f620d156
sadaan <ansarisadaan72@gmail.com>
2024-03-06 14:07:35 +0530

    design profile section

---
+++ src/components/profile/Profile.jsx
@@ -0,0 +1,64 @@
+import './Profile.scss';
+import {motion} from 'framer-motion'
+const textVariants = {
+    initial: {
+        x: -500,
+        opacity: 0,
+    },
+    animate: {
+        x: 0,
+        opacity: 1,
+        transition: {
+            duration: 1,
+            staggerChildren: 0.1,
+        }
+    },
+    scrollButton: {
+        opacity: 0,
+        y:10,
+        transition: {
+            duration: 2,
+            repeat: Infinity,
+        }
+    }
+};
+
+const SliderVariants = {
+    initial: {
+        x: 0,
+    },
+    animate: {
+        x: "-220%",
+        transition: {
+            repeat: Infinity,
+            repeatType: "mirror",
+            duration: 20,
+        }
+    },
+};
+
+const Profile = () => {
+    return (
+        <div className='profile'>
+            <div className="wrapper">
+                <motion.div className="textContainer" variants={textVariants} initial='initial' animate='animate'>
+                    <motion.h2 variants={textVariants}>ANSARI SADAAN</motion.h2>
+                    <motion.h1 variants={textVariants}>Web Developer and MERN Developer</motion.h1>
+                    <motion.div variants={textVariants} className="buttons">
+                        <motion.button variants={textVariants}>Download CV</motion.button>
+                        <motion.button variants={textVariants}>Contact Me</motion.button>
+                    </motion.div>
+                    <motion.img variants={textVariants} animate='scrollButton' src="scroll.png" alt="" />
+                </motion.div>
+            </div>
+            <motion.div className="slidingTextContainer" variants={SliderVariants} initial='initial' animate='animate'>
+                Website Application Content 
+            </motion.div>
+            <div className="imageContainer">
+                <img src="profile.png" alt="" />
+            </div>
+        </div>
+    )
+}
+
+export default Profile
