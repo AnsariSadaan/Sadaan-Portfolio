2dc5015aa81dfb3a6b8fa24b5fa715d07ace14f4
sadaan <ansarisadaan72@gmail.com>
2024-03-06 14:04:01 +0530

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 import './App.scss';
 // import Test from './Test';
 import Navbar from './components/navbar/Navbar';
+import Parallax from './components/parallax/Parallax';
+import Profile from './components/profile/Profile';
 
 function App() {
   
   return (
       <div>
-        <section id='Homepage'><Navbar /></section>
-        <section>Parallex</section>
+        <section id='Homepage'>
+          <Navbar />
+          <Profile />
+        </section>
+        <section id='services'><Parallax type="services" /></section>
         <section id='Services'>Services</section>
-        <section >Parallex</section>
-        <section id='Portfolio'>Portfolio 1</section>
+        <section id='portfolio'><Parallax type="portfolio" /></section>
+        <section>Portfolio 1</section>
         <section>Portfolio 2</section>
         <section>Portfolio 3</section>
         <section id='Contact'>Contact</section>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
                 initial={{opacity: 0, scale: 0.5}}
                 animate={{ opacity: 1, scale: 1 }}
                 transition={{duration: 0.5}}
-                >Ansari Sadaan</motion.span>
+                >AS.</motion.span>
                 <div className='social'>
                     <a href=""><img src="/linkedin.png" alt="" /></a>
                     <a href=""><img src="/instagram.png" alt="" /></a>
